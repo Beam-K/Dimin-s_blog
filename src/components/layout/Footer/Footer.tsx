0c9b47746d08bd009c007e700e1ab16078103a47
Beam-K <Kbuldina415@gmail.com>
2025-12-16 21:11:10 +0700

    .

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import styles from './Footer.module.css';
-import tg from "../../../assets/images/tg.png"
-import githab from "../../../assets/images/githab.png"
-import {Link} from "react-router-dom";
 
 
 function Footer() {
